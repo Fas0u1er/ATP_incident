e0289967ae790f04f3f5b2dc2ae479513372bddc
Fas0u1er <prokhorov.bi@phystech.edu>
2022-03-19 22:03:03 +0300

    GUI_Interface.h make singleton

--- GUI_Interface.h
+++ GUI_Interface.h
@@ -4,7 +4,8 @@ private:
     GUI_Interface() = default;
 public:
     static GUI_Interface& getInstance() {
-
+        static GUI_Interface gui;
+        return gui;
     }
     virtual ~GUI_Interface() = default;
 };
